8c5f8d4da03ba09d7f1655afb66200171b362b5f
Alvin Goh <alvinaloy@hotmail.com>
2014-08-24 20:17:46 +0900

    2nd commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 ## makeCacheMatrix creates a special "vector", which is really a list containing a function to
 ## set the value of the matrix
 ## get the value of the matrix
@@ -22,7 +21,6 @@ makeCacheMatrix <- function(x = matrix()) {
         list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
 }
 
-## Write a short comment describing this function
 ## cacheSolve calculates the inverse of the special "vector" created with the above function.
 ## It first checks to see if the inverse matrix has already been calculated.
 ## If so, it gets the inverse matrix from the cache and skips the computation.
